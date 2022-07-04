19f33e075f53b523d07263e492d1e86fd2e6d33b
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-07-04 09:01:09 -0400

    updating code to PEP8

--- modules/Calendar.py
+++ modules/Calendar.py
@@ -1,73 +1,114 @@
-from modules.Scheduler import Schedule, Timeslot, convert_idx_time, Availability
+'''
+A calendar class to represent the availability of ALL users.
+'''
+from scheduler import Schedule
+from utilities import Timeslot, convert_idx_time, Availability
+
 
 class Calendar():
-  def __init__(self):
-    self.calendar = {}
-    self.master = {}
-    self.people = 0
+    '''
+    Calendar class
+    '''
 
-    for day in ['Monday', "Tuesday", 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
-      self.master[day] = [0] * 24
+    def __init__(self):
+        self.calendar = {}
+        self.master = {}
+        self.people = 0
 
-  def reset_master(self):
-    for day in self.master:
-      self.master[day] = [0] * 24
+        for day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
+            self.master[day] = [0] * 24
 
-  def get_coverage(self, count) -> int:
-    return round((count / self.people) * 100, 2)
+    def reset_master(self):
+        '''
+        Clear data in the calendar
+        '''
+        for day in self.master:
+            self.master[day] = [0] * 24
 
-  def add_user(self, user: str):
-    self.calendar[user] = Schedule()
-    self.people += 1
+    def get_coverage(self, count) -> int:
+        '''
+        Will returnt the percentage of people available for that timeslot
+        '''
+        return round((count / self.people) * 100, 2)
 
-  def usr_sched_as_str(self, user:str) -> str:
-    if user not in self.calendar: return 'No Data Found'
-    return self.calendar[user].get_schedule()
+    def add_user(self, user: str):
+        '''
+        Add a new user by create a schedule for them
+        '''
+        self.calendar[user] = Schedule()
+        self.people += 1
 
-  def get_avail_days(self, user:str) ->str:
-    if user not in self.calendar: return "Input Availability"
-    return self.calendar[user].days_done()
+    def usr_sched_as_str(self, user: str) -> str:
+        '''
+        Return the specified user's schedule as a string
+        '''
+        if user not in self.calendar:
+            return 'No Data Found'
+        return self.calendar[user].get_schedule()
 
-  def get_schedule(self, user: str) -> Schedule:
-    return self.calendar[user]
+    def get_avail_days(self, user: str) -> str:
+        '''
+        Return the days that the user has available
+        '''
+        if user not in self.calendar:
+            return 'Input Availability'
+        return self.calendar[user].days_done()
 
-  def update_master(self):
-    self.reset_master()
-    for user in self.calendar:
-      for day in self.get_schedule(user).data():
-        for i, status in enumerate(self.get_schedule(user).data()[day]):
-          if status == Availability.FREE:
-            self.master[day][i] += 1
+    def get_schedule(self, user: str) -> Schedule:
+        '''
+        Return the use's Schedule
+        '''
+        return self.calendar[user]
 
-  def find_best(self) -> str:
-    if self.people == 0: return 'No Responses'
+    def update_master(self):
+        '''
+        Update the master calendar with the current availability
+        based on each individual user's schedule
+        '''
+        self.reset_master()
+        for user in self.calendar:
+            for day in self.get_schedule(user).data():
+                for i, status in enumerate(self.get_schedule(user).data()[day]):
+                    if status == Availability.FREE:
+                        self.master[day][i] += 1
 
-    rankings = []
-    for day, status in self.master.items():
-      for time, count in enumerate(status):
-        if count > 0:
-          rankings.append(
-            Timeslot(day, convert_idx_time(time), count)
-          )
+    def find_best(self) -> str:
+        '''
+        Find the best timeslot to schedule a user
+        '''
+        if self.people == 0:
+            return 'No Responses'
 
-    if not rankings: return 'No Available Slots'
+        rankings = []
+        for day, status in self.master.items():
+            for time, count in enumerate(status):
+                if count > 0:
+                    rankings.append(
+                        Timeslot(day, convert_idx_time(time), count)
+                    )
 
-    rankings.sort(key=lambda t: t.count, reverse=True)
+        if not rankings:
+            return 'No Available Slots'
 
-    res = f"Respones = {self.people}\n"
-    current = None
-    for timeslot in rankings:
-      if current != timeslot.count:
-        current = timeslot.count
-        res += f"\n__Slots with coverage of **{self.get_coverage(current)}%**:__\n"
-        res += f"[{timeslot.to_string()}], \t" 
-      else: 
-        res += f"[{timeslot.to_string()}], \t"
+        rankings.sort(key=lambda t: t.count, reverse=True)
 
-    return res[:-3]
+        res = f'Respones = {self.people}\n'
+        current = None
+        for timeslot in rankings:
+            if current != timeslot.count:
+                current = timeslot.count
+                res += f'\n__Slots with coverage of **{self.get_coverage(current)}%**:__\n'
+                res += f'[{timeslot.to_string()}], \t'
+            else:
+                res += f'[{timeslot.to_string()}], \t'
 
-  def update(self, user, day, times):
-    if user not in self.calendar: 
-      self.add_user(user)
-    self.get_schedule(user).update(day, times)
-    self.update_master()
+        return res[:-3]
+
+    def update(self, user, day, times):
+        '''
+        Update if a user's wants to change previously inputted data
+        '''
+        if user not in self.calendar:
+            self.add_user(user)
+        self.get_schedule(user).update(day, times)
+        self.update_master()
--- modules/Scheduler.py
+++ modules/Scheduler.py
@@ -1,44 +1,63 @@
-from modules.Utilities import Availability, convert_idx_time
+'''
+Class represnting a schduler.
+This will encapulsate teh avialble for ONE user
+'''
 from typing import List
+from utilities import Availability, convert_idx_time
+
 
 class Schedule():
-  def __init__(self):
-    self.schedule = {}
-    for day in ['Monday', "Tuesday", 'Wednesay', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
-      self.schedule[day] = [Availability.BUSY] * 24
+    '''
+    Class for scheduling a user
+    '''
 
-  def data(self):
-    return self.schedule
+    def __init__(self):
+        self.schedule = {}
+        for day in ['Monday', 'Tuesday', 'Wednesay', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
+            self.schedule[day] = [Availability.BUSY] * 24
 
-  def reset(self, day):
-      self.schedule[day] = [Availability.BUSY] * 24
+    def data(self) -> dict[str, Availability]:
+        '''
+        Return schedule data as a map
+        '''
+        return self.schedule
 
-  def days_done(self) -> str:
-    res = "Days With At Least One Available Slot:\n"
-    for day, status in self.schedule.items():
-      if Availability.FREE in status:
-        res += f" | {day}: ✅ | "
-    return res
+    def reset(self, day):
+        '''
+        Clears the schedule for the day (sets every time to busy)
+        '''
+        self.schedule[day] = [Availability.BUSY] * 24
 
-  def update(self, day: str, times: List[str]):
-    self.reset(day)
-    for time in times:
-      self.schedule[day][int(time)] = Availability.FREE
+    def days_done(self) -> str:
+        '''
+        Will output a string of the days that the user has completed
+        '''
+        res = 'Days With At Least One Available Slot:\n'
+        for day, status in self.schedule.items():
+            if Availability.FREE in status:
+                res += f' | {day}: ✅ | '
+        return res
 
-  def get_schedule(self) -> str:
-    res = ""
-    for day, status in self.schedule.items():
-      if Availability.FREE in status:
-        res += f"Day: {day} \n"
-        for time, avail in enumerate(status):
-          if avail == Availability.FREE:
-            res += f"[{convert_idx_time(time)}], "
-        res += '\n'
+    def update(self, day: str, times: List[str]):
+        '''
+        Will update the schedule with the given times
+        that the user is available for
+        '''
+        self.reset(day)
+        for time in times:
+            self.schedule[day][int(time)] = Availability.FREE
 
-    return res if res else "No Times Found"
+    def get_schedule(self) -> str:
+        '''
+        Will return the schedule as a string
+        '''
+        res = ''
+        for day, status in self.schedule.items():
+            if Availability.FREE in status:
+                res += f'Day: {day} \n'
+                for time, avail in enumerate(status):
+                    if avail == Availability.FREE:
+                        res += f'[{convert_idx_time(time)}], '
+                res += '\n'
 
-  def print(self):
-    for day in self.schedule:
-      print(f'{day}:\n self.schedule[day]\n')
-
-
+        return res if res else 'No Times Found'
